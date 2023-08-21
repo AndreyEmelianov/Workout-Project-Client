99780584a1333cc833579fff82adcdfe0476c5bc
Andrey Emelianov <621077@mail.ru>
2023-08-21 16:29:44 +0300

    добавил иконку для страницы профиля, и настроил роутинг на страницу профиля по клику на иконку

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,17 +1,36 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth';
 import Hamburger from '../hamburger/Hamburger';
 
 import styles from './Header.module.scss';
 import { IoMdArrowBack } from 'react-icons/io';
+import { SlUser } from 'react-icons/sl';
 
-const Header = ({ backLink }) => {
+const Header = ({ backLink = '' }) => {
+	const { pathname } = useLocation();
+	const navigate = useNavigate();
+
 	const { isAuth } = useAuth();
 
 	return (
 		<header className={styles.header}>
-			<button onClick={() => {}}>
-				<IoMdArrowBack fill='#fff' fontSize={28} />
-			</button>
+			{pathname !== '/' ? (
+				<button
+					onClick={() => {
+						navigate(backLink);
+					}}
+				>
+					<IoMdArrowBack fill='#fff' fontSize={28} />
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						navigate('/profile');
+					}}
+				>
+					<SlUser fill='#fff' fontSize={25} />
+				</button>
+			)}
 			<Hamburger />
 		</header>
 	);
