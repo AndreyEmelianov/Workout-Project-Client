89ab3e7de37978e705f5266b51a477f35ce1a1d2
Andrey Emelianov <621077@mail.ru>
2023-08-21 17:22:37 +0300

    сделал компонент кнопки, добавил изображения, добавил кнопку и bg на главный экран Home

--- src/components/screens/not-found/NotFound.jsx
+++ src/components/screens/not-found/NotFound.jsx
@@ -3,7 +3,7 @@ import Layout from '../../layout/Layout';
 const NotFound = () => {
 	return (
 		<>
-			<Layout heading='Page not found' />;
+			<Layout heading='Page not found' />
 			<div className='wrapper-inner-page'>404 page not found</div>
 		</>
 	);
