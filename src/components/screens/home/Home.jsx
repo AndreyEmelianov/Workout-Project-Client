5c091bd75f7495e5d4b65d4d125b8d2bea2b8896
Andrey Emelianov <621077@mail.ru>
2023-08-21 15:54:34 +0300

    Добавил роутинг для страниц используя react-router-dom

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,11 +1,7 @@
 import Layout from '../../layout/Layout';
 
 function Home() {
-	return (
-		<Layout>
-			<div>Workout</div>
-		</Layout>
-	);
+	return <Layout></Layout>;
 }
 
 export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Home from './components/screens/home/Home';
+import Router from './routes/Routes';
 
 import './assets/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<Home />
+		<Router />
 	</React.StrictMode>
 );
