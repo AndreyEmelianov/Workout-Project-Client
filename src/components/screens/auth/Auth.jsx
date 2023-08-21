4e89d4a802e3e9e27081a30cfec9820bd5b5984a
Andrey Emelianov <621077@mail.ru>
2023-08-21 18:22:47 +0300

    реализовал компонент инпута Field добавил два поля email и password в форму на странице авторизации Auth, добавил кнопки, добавил компонент лоадер

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,9 +1,25 @@
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+
 import Layout from '../../layout/Layout';
 import Button from '../../ui/button/Button';
+import Field from '../../ui/field/Field';
+import Loader from '../../ui/loader/Loader';
+
+import styles from './Auth.module.scss';
+
+//временное состояние лоадера
+const isLoading = false;
+const isLoadingAuth = false;
 
 const Auth = () => {
-	const { register, handleSubmit } = useForm({
+	const [type, setType] = useState('auth');
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
 		mode: 'onChange'
 	});
 
@@ -12,20 +28,38 @@ const Auth = () => {
 	};
 
 	return (
-		<Layout heading='Sign in' bgImage='/images/auth-bg.png'>
+		<>
+			<Layout heading='Sign in' bgImage='/images/auth-bg.png' />
 			<div className='wrapper-inner-page'>
+				{(isLoading || isLoadingAuth) && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
-					<input
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{
+							required: 'Email is required'
+						}}
 						type='text'
 						placeholder='Enter email'
-						{...register('email', {
-							required: 'Email is required'
-						})}
 					/>
-					<Button>Let's go</Button>
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{
+							required: 'Password is required'
+						}}
+						type='password'
+						placeholder='Enter password'
+					/>
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+					</div>
 				</form>
 			</div>
-		</Layout>
+		</>
 	);
 };
 
