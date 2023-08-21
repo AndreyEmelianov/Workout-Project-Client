7291fabe0ace744889ece4247d70e84a0f3910a4
Andrey Emelianov <621077@mail.ru>
2023-08-21 17:48:58 +0300

    подключил react-hook-form и начал реализацию компонента авторизации Auth

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,5 +1,32 @@
+import { useForm } from 'react-hook-form';
+import Layout from '../../layout/Layout';
+import Button from '../../ui/button/Button';
+
 const Auth = () => {
-	return <div>Auth</div>;
+	const { register, handleSubmit } = useForm({
+		mode: 'onChange'
+	});
+
+	const onSubmit = data => {
+		console.log(data);
+	};
+
+	return (
+		<Layout heading='Sign in' bgImage='/images/auth-bg.png'>
+			<div className='wrapper-inner-page'>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type='text'
+						placeholder='Enter email'
+						{...register('email', {
+							required: 'Email is required'
+						})}
+					/>
+					<Button>Let's go</Button>
+				</form>
+			</div>
+		</Layout>
+	);
 };
 
 export default Auth;
