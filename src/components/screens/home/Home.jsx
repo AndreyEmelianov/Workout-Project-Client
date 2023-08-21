55361f13caaf08746e630327b1c0ac853a44d572
Andrey Emelianov <621077@mail.ru>
2023-08-21 15:22:19 +0300

    начал реализовывать основыне роуты и создал заглушки компонентов для этих роутов

---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../layout/Layout';
+
+function Home() {
+	return (
+		<Layout>
+			<div>Workout</div>
+		</Layout>
+	);
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import Home from './components/screens/home/Home';
+
 import './assets/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<Home />
 	</React.StrictMode>
 );
