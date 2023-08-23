f02077bff80aebbd80d467c59f18a042761aa0e6
Andrey Emelianov <621077@mail.ru>
2023-08-23 16:49:06 +0300

    добавил блок картинок в профиле юзера before after

--- src/components/screens/profile/Profile.jsx
+++ src/components/screens/profile/Profile.jsx
@@ -12,27 +12,52 @@ const Profile = () => {
 	const { data, isLoading } = useProfile();
 
 	return (
-		<div
-			className={cn(stylesLayout.wrapper, stylesLayout.otherPage)}
-			style={{ backgroundImage: `url('/images/profile-bg.jpg')`, height: 356 }}
-		>
-			<Header />
-			<div className={styles.center}>
-				{isLoading ? (
-					<Loader />
-				) : (
-					<>
-						<img
-							src='images/header/user.svg'
-							alt='Profile'
-							height='56'
-							draggable={false}
-						></img>
-						<h1 className={stylesLayout.heading}>{data?.email}</h1>
-					</>
-				)}
+		<>
+			<div
+				className={cn(stylesLayout.wrapper, stylesLayout.otherPage)}
+				style={{
+					backgroundImage: `url('/images/profile-bg.jpg')`,
+					height: 356
+				}}
+			>
+				<Header />
+				<div className={styles.center}>
+					{isLoading ? (
+						<Loader />
+					) : (
+						<>
+							<img
+								src='images/header/user.svg'
+								alt='Profile'
+								height='56'
+								draggable={false}
+							></img>
+							<h1 className={stylesLayout.heading}>{data?.email}</h1>
+						</>
+					)}
+				</div>
 			</div>
-		</div>
+
+			<div
+				className='wrapper-inner-page'
+				style={{ paddingLeft: 0, paddingRight: 0 }}
+			>
+				<div className={styles.before_after}>
+					{data?.images?.map((image, index) => (
+						<div key={index}>
+							<div className={styles.heading}>
+								{index === 1 ? 'After' : 'Before'}
+							</div>
+							<img
+								src={image}
+								alt={index === 1 ? 'After' : 'Before'}
+								draggable={false}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+		</>
 	);
 };
 
