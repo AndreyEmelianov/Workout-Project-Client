30fe51eecfb4a18c924a9fcb617ef7dacca54b30
Andrey Emelianov <621077@mail.ru>
2023-08-23 18:32:22 +0300

    добавил сервис по тренировке по аналогии с сервисом упражнений, (получение, получение по id, создание, обновление и удаление)

---
+++ src/services/workout/workout.service.js
@@ -0,0 +1,28 @@
+import { $axios } from '../../api';
+
+const WORKOUTS = '/workout';
+
+class WorkoutService {
+	async getAll() {
+		return $axios.get(WORKOUTS);
+	}
+
+	async getById(id) {
+		return $axios.get(`${WORKOUTS}/${id}`);
+	}
+
+	// body - name, exerciseId
+	async create(body) {
+		return $axios.post(WORKOUTS, body);
+	}
+
+	async update(id, body) {
+		return $axios.put(`${WORKOUTS}/${id}`, body);
+	}
+
+	async delete(id) {
+		return $axios.delete(`${WORKOUTS}/${id}`);
+	}
+}
+
+export default new WorkoutService();
