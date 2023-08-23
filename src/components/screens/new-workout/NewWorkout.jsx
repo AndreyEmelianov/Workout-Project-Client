14be3242d8fde32a9c1f6838bfaccbd1dc9df048
Andrey Emelianov <621077@mail.ru>
2023-08-23 18:43:35 +0300

    начал реализацию компонента  с новой тренировкой new Workout, вынес в отдельные файлы логику работы с формой, с мутацией и в отдельный файл получение списка упражнений

--- src/components/screens/new-workout/NewWorkout.jsx
+++ src/components/screens/new-workout/NewWorkout.jsx
@@ -1,5 +1,84 @@
+import { useMutation } from '@tanstack/react-query';
+import { useForm, Controller } from 'react-hook-form';
+import cn from 'clsx';
+
+import WorkoutService from '../../../services/workout/workout.service';
+import Layout from '../../layout/Layout';
+import Loader from '../../ui/loader/Loader';
+import Button from '../../ui/button/Button';
+import Alert from '../../ui/alert/Alert';
+
+import styles from './NewWorkout.module.scss';
+import Field from '../../ui/field/Field';
+
 const NewWorkout = () => {
-	return <div>NewWorkout</div>;
+
+
+	return (
+		<>
+			<Layout
+				bgImage='/images/new-workout-bg.jpg'
+				heading='Create new workout'
+			/>
+			<div className='wrapper-inner-page'>
+				{error && <Alert type='error' text={error} />}
+				{isSuccess && <Alert text='Workout created' />}
+				{isLoading && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						error={errors?.name?.message}
+						name='name'
+						register={register}
+						options={{
+							required: 'Name is required'
+						}}
+						type='text'
+						placeholder='Enter name'
+					/>
+					<Field
+						error={errors?.times?.message}
+						name='times'
+						register={register}
+						options={{
+							valueAsNumber: true,
+							required: 'Times is required',
+							validate: value => value > 0 || 'Times must be number'
+						}}
+						placeholder='Enter times'
+					/>
+
+					<Controller
+						name='iconPath'
+						control={control}
+						render={({ field: { value, onChange } }) => (
+							<div className={styles.images}>
+								{data.map(name => (
+									<img
+										key={`ex img ${name}`}
+										src={`${import.meta.env.VITE_SERVER_URL}${getIconPath(
+											name
+										)}`}
+										alt={name}
+										className={cn({
+											[styles.active]: value === getIconPath(name)
+										})}
+										onClick={() => onChange(getIconPath(name))}
+										draggable={false}
+										height='45'
+									></img>
+								))}
+							</div>
+						)}
+					/>
+
+					{errors?.iconPath && (
+						<div className='error'>{errors?.iconPath?.message}</div>
+					)}
+					<Button>Create</Button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default NewWorkout;
