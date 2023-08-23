c94b3a432efe4f2ed597e835f45ca4a7c1a3acf9
Andrey Emelianov <621077@mail.ru>
2023-08-23 18:26:48 +0300

    добавил env файл вынес пути получения изображений упражнений в отельную утилиту, используя useForm, добавил форму добавления упражнений в тренировке, добавил алерты с оповщениями об ошибке, об успешно записаном упражнении

--- src/components/screens/new-exercise/NewExercise.jsx
+++ src/components/screens/new-exercise/NewExercise.jsx
@@ -1,13 +1,110 @@
 import { useMutation } from '@tanstack/react-query';
-import styles from './NewExercise.module.scss';
+import { useForm, Controller } from 'react-hook-form';
+import cn from 'clsx';
+
 import ExerciseService from '../../../services/exercise/exercise.service';
+import Layout from '../../layout/Layout';
+import Loader from '../../ui/loader/Loader';
+import Button from '../../ui/button/Button';
+import Alert from '../../ui/alert/Alert';
+
+import styles from './NewExercise.module.scss';
+import Field from '../../ui/field/Field';
+import { getIconPath } from './image-path.util';
+
+const data = ['chest', 'shoulders', 'biceps', 'legs', 'hit', 'back'];
 
 const NewExercise = () => {
-	const { isSuccess, error } = useMutation(['create exercise'], body =>
-		ExerciseService.create(body)
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+		control
+	} = useForm({
+		mode: 'onChange'
+	});
+
+	const { isSuccess, error, isLoading, mutate } = useMutation(
+		['create exercise'],
+		body => ExerciseService.create(body),
+		{
+			onSuccess: () => {
+				reset();
+			}
+		}
 	);
 
-	return <div>NewExercise</div>;
+	const onSubmit = data => {
+		mutate(data);
+	};
+
+	return (
+		<>
+			<Layout
+				bgImage='/images/new-exercise-bg.jpg'
+				heading='Create new exercise'
+				backLink='/new-workout'
+			/>
+			<div className='wrapper-inner-page'>
+				{error && <Alert type='error' text={error} />}
+				{isSuccess && <Alert text='Exercise created' />}
+				{isLoading && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						error={errors?.name?.message}
+						name='name'
+						register={register}
+						options={{
+							required: 'Name is required'
+						}}
+						type='text'
+						placeholder='Enter name'
+					/>
+					<Field
+						error={errors?.times?.message}
+						name='times'
+						register={register}
+						options={{
+							valueAsNumber: true,
+							required: 'Times is required',
+							validate: value => value > 0 || 'Times must be number'
+						}}
+						placeholder='Enter times'
+					/>
+
+					<Controller
+						name='iconPath'
+						control={control}
+						render={({ field: { value, onChange } }) => (
+							<div className={styles.images}>
+								{data.map(name => (
+									<img
+										key={`ex img ${name}`}
+										src={`${import.meta.env.VITE_SERVER_URL}${getIconPath(
+											name
+										)}`}
+										alt={name}
+										className={cn({
+											[styles.active]: value === getIconPath(name)
+										})}
+										onClick={() => onChange(getIconPath(name))}
+										draggable={false}
+										height='45'
+									></img>
+								))}
+							</div>
+						)}
+					/>
+
+					{errors?.iconPath && (
+						<div className='error'>{errors?.iconPath?.message}</div>
+					)}
+					<Button>Create</Button>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default NewExercise;
