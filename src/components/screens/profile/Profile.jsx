0569e8ccffd39c661cfaa11f1f2c6c256942d757
Andrey Emelianov <621077@mail.ru>
2023-08-23 16:28:53 +0300

    начал реализацию компонента профиля юзера и добавил стили

--- src/components/screens/profile/Profile.jsx
+++ src/components/screens/profile/Profile.jsx
@@ -1,7 +1,39 @@
 import Layout from '../../layout/Layout';
+import cn from 'clsx';
+
+import { useProfile } from './useProfile';
+
+import stylesLayout from '../../layout/Layout.module.scss';
+import styles from './Profile.module.scss';
+import Header from '../../layout/header/Header';
+import Loader from '../../ui/loader/Loader';
 
 const Profile = () => {
-	return <Layout>Profile</Layout>;
+	const { data, isLoading } = useProfile();
+
+	return (
+		<div
+			className={cn(stylesLayout.wrapper, stylesLayout.otherPage)}
+			style={{ backgroundImage: `url('/images/profile-bg.jpg')`, height: 356 }}
+		>
+			<Header />
+			<div className={styles.center}>
+				{isLoading ? (
+					<Loader />
+				) : (
+					<>
+						<img
+							src='images/header/user.svg'
+							alt='Profile'
+							height='56'
+							draggable={false}
+						></img>
+						<h1 className={stylesLayout.heading}>{data?.email}</h1>
+					</>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
