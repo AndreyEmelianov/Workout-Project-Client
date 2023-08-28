4ca8689e62326f0e845128b1209202a8bacefd97
Andrey Emelianov <621077@mail.ru>
2023-08-28 12:29:17 +0300

    начал реализацию компонена логов упражнений ExerciseLog. разбил на подкомпоненты, реализовал хедер HeaderExerciseLog

--- src/components/screens/exercise-log/ExerciseLog.jsx
+++ src/components/screens/exercise-log/ExerciseLog.jsx
@@ -1,5 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import cn from 'clsx';
+
+import ExerciseLogService from '../../../services/exercise/exercise-log.service';
+
+import styles from './ExerciseLog.module.scss';
+import Header from '../../layout/header/Header';
+import HeaderExerciseLog from './HeaderExerciseLog';
+
 const ExerciseLog = () => {
-	return <div>ExerciseLog</div>;
+	const { id } = useParams();
+
+	const {
+		data: exerciseLog,
+		isSuccess,
+		refetch,
+		isLoading
+	} = useQuery(['get exercise log', id], () => ExerciseLogService.getById(id), {
+		select: ({ data }) => data
+	});
+
+	return (
+		<>
+			<HeaderExerciseLog exerciseLog={exerciseLog} isSuccess={isSuccess} />
+		</>
+	);
 };
 
 export default ExerciseLog;
